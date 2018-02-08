3c1d859525d8764b5114b9fd221ff153c1550fea
mustafa-synectiks <mustafa.mohammed@synectiks.com>
2018-02-08 17:19:16 +0530

    using hash true

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -29,7 +29,7 @@ export const appRoutes: Routes  =  [
 ];
 
 @NgModule({
-imports: [RouterModule.forRoot(appRoutes, { useHash: false } )],
+imports: [RouterModule.forRoot(appRoutes, { useHash: true } )],
   exports: [RouterModule]
 })
 
